84f9f8840f96016c495ec3b805822ba3de7bae92
T x <emraslanovsupan290@gmail.com>
2023-12-28 09:29:36 +0400

    Change Email register minlenth 25 to 10

--- PesKit/PesKit/ViewModels/Account/RegisterVM.cs
+++ PesKit/PesKit/ViewModels/Account/RegisterVM.cs
@@ -20,7 +20,7 @@ namespace PesKit.ViewModels
         public string Surname { get; set; }
 
         [Required(ErrorMessage = "Email must be entered mutled")]
-        [MinLength(25, ErrorMessage = "It should not exceed 25-255 characters")]
+        [MinLength(10, ErrorMessage = "It should not exceed 10-255 characters")]
         [MaxLength(255, ErrorMessage = "It should not exceed 25-255 characters")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
